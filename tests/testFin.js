d5f133cc0bd10de64d2da84309e89f17fc8cb902
Racer25 <charles.cousyn1@uqac.ca>
2020-11-23 10:46:18 -0500

    Test du dependency parser

--- tests/testFin.js
+++ tests/testFin.js
@@ -1,5 +1,7 @@
 import Lexed from 'lexed';
 import {Tag} from 'en-pos';
+import parser from 'en-parse';
+import clone from "clone";
 import {normalizeCaps,replaceConfusables,resolveContractions} from "en-norm";
 import Wordpos from "wordpos";
 import { Inflectors } from "en-inflectors";
@@ -23,5 +25,26 @@ import { Inflectors } from "en-inflectors";
 	let res12 = new Inflectors("updated").conjugate("VBP");
 
 	console.log(res12);
+
+	let cleanText = "Before killing you, i kill your father."
+	let tokenizedText = (new Lexed(cleanText)).lexer().tokens;
+	let clonedTokenizedText = clone(tokenizedText);
+	let normalizedTokenizedText = clonedTokenizedText.map(sentenceArr => normalizeCaps(sentenceArr));
+	let POSText = normalizedTokenizedText.map(sentenceArr => new Tag(sentenceArr).initial().smooth().tags);
+	let depParsed = normalizedTokenizedText.map((tokensOneSentence, indexOneSentence) => parser(POSText[indexOneSentence], tokensOneSentence));
+
+	let processedSentences = normalizedTokenizedText.map((normalizedTokensOneSentence, indexOneSentence) =>
+		normalizedTokensOneSentence.map((normalizedToken, indexTok) =>
+		{
+			return {
+				indexSentence: indexOneSentence,
+				indexToken: indexTok,
+				originalToken: tokenizedText[indexOneSentence][indexTok],
+				normalizedToken: normalizedToken.toLowerCase(),
+				POS: POSText[indexOneSentence][indexTok],
+				depParsed: depParsed[indexOneSentence][indexTok]
+			}}));
+
+	console.log(processedSentences);
 
 })();
